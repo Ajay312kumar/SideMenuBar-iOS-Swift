b1698c2cb57960bcbab3066569b9f4abb2eab647
Ajay Kumar <124868129+ajay-kumar-iphtech@users.noreply.github.com>
2024-01-31 01:29:14 +0530

    left and right swipe added

--- SideMenuBar/ViewController.swift
+++ SideMenuBar/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  SideMenuBar
-//
-//  Created by Ajay Kumar on 30/01/24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -14,47 +7,126 @@ class ViewController: UIViewController {
     var sideBarView: UIView!
     var tableView:UITableView!
     var isEnableSideBarView:Bool = false
+    var imgArray: [String] = ["dashboard", "revanue", "notification", "like", "wallet"]
+    var imgeNameLbl = ["Dashboard", "Revanue", "Notification", "Like", "Wallet"]
+    var imgView: UIImageView!
+    var lbl: UILabel!
     
-    
+    var swipToRight = UISwipeGestureRecognizer()
+    var swipeToLeft = UISwipeGestureRecognizer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
-        var menuBtn = UIBarButtonItem(title:"Menu", style: .done, target: self, action: #selector(menuBtnClick))
+        let menuBtn = UIBarButtonItem(title: "Menu", style: .done, target: self, action: #selector(menuBtnClick))
         self.navigationItem.leftBarButtonItem = menuBtn
         
         sideBarView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: self.view.bounds.height))
         
         tableView = UITableView(frame: CGRect(x: 0, y: 0, width: 0, height: self.view.bounds.height))
         
+        tableView.backgroundColor = .lightGray
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
         self.view.addSubview(sideBarView)
         self.sideBarView.addSubview(tableView)
+        
+        swipToRight = UISwipeGestureRecognizer(target: self, action: #selector(swipedToRight))
+        swipToRight.direction = .right
+        self.view.addGestureRecognizer(swipToRight)
+        
+        swipeToLeft = UISwipeGestureRecognizer(target: self, action: #selector(swipedToLeft))
+        swipeToLeft.direction = .left
+//        self.view.addGestureRecognizer(swipeToLeft)
+        
     }
 
+    
+    @objc func swipedToLeft() {
+        self.view.addGestureRecognizer(swipToRight)
+        self.view.removeGestureRecognizer(swipeToLeft)
+        UIView.animate(withDuration: 0.5) {
+            self.sideBarView.frame = CGRect(x: 0, y: 50, width: 0, height: self.view.bounds.height)
+            self.tableView.frame = CGRect(x: 0, y: 50, width: 0, height: self.view.bounds.height)
+        }
+        isEnableSideBarView = false
+
+        
+    }
+    
+    
+    @objc func swipedToRight() {
+        self.view.addGestureRecognizer(swipeToLeft)
+        self.view.removeGestureRecognizer(swipToRight)
+        UIView.animate(withDuration: 0.5) {
+            self.sideBarView.frame = CGRect(x: 0, y: 50, width: self.view.bounds.width/1.5, height: self.view.bounds.height)
+            self.tableView.frame = CGRect(x: 0, y: 50, width: self.view.bounds.width/1.5, height: self.view.bounds.height)
+            
+        }
+        isEnableSideBarView = true
+    }
+    
+    
+    
+    
     
     @objc func menuBtnClick() {
         print("btn click")
         
         if isEnableSideBarView {
-
-           // UIView.animate(withDuration: 0.5){
-                self.sideBarView.frame = CGRect(x: self.view.bounds.width, y: 50, width: 0, height: self.view.bounds.height)
-                self.tableView.frame = CGRect(x: 0, y: 50, width: 0, height: self.view.bounds.height)
-                self.isEnableSideBarView = false
-          //  }
-        } else {
-            // Show the side menu by bringing it on-screen
             
-         //   UIView.animate(withDuration: 0.5){
+            self.view.addGestureRecognizer(swipToRight)
+            self.view.removeGestureRecognizer(swipeToLeft)
+            UIView.animate(withDuration: 0.5) {
+                self.sideBarView.frame = CGRect(x: 0, y: 50, width: 0, height: self.view.bounds.height)
+                self.tableView.frame = CGRect(x: 0, y: 50, width: 0, height: self.view.bounds.height)
+            }
+            isEnableSideBarView = false
+
+            
+        } else {
+            
+            self.view.addGestureRecognizer(swipeToLeft)
+            self.view.removeGestureRecognizer(swipToRight)
+
+            UIView.animate(withDuration: 0.5) {
                 self.sideBarView.frame = CGRect(x: 0, y: 50, width: self.view.bounds.width/1.5, height: self.view.bounds.height)
                 self.tableView.frame = CGRect(x: 0, y: 50, width: self.view.bounds.width/1.5, height: self.view.bounds.height)
-                self.isEnableSideBarView = true
-          //  }
+            }
+            isEnableSideBarView = true
+
         }
     }
-
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imgArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        imgView = UIImageView(frame: CGRect(x: 8, y: 8, width: cell.bounds.height-16, height: cell.bounds.height-16))
+        imgView.contentMode = .scaleToFill
+        
+        if let image = UIImage(named: self.imgArray[indexPath.row]) {
+            imgView.image = image
+        } else {
+            // Handle the case where the image is not found
+            imgView.image = nil
+        }
+        
+        cell.addSubview(imgView)
+        
+        lbl = UILabel(frame: CGRect(x: imgView.frame.maxX + 16, y: 8, width: cell.bounds.width - (imgView.frame.maxX + 24), height: cell.bounds.height-16))
+        lbl.text = "\(imgeNameLbl[indexPath.row])"
+        
+        cell.addSubview(lbl)
+        
+        return cell
+    }
+}
